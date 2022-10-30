e64eec623f1b745b226a3fabe776b771bf7c08c7
astranero <esa.taheresa@gmail.com>
2022-10-30 15:07:07 +0200

    8th on done

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,17 +10,40 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_lisataan_negatiivinen_tavaraa(self):
+        self.assertAlmostEqual(self.varasto.lisaa_varastoon(-10), None)
+
+    def test_otetaan_negatiivinen_varastosta(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-10), 0)
+
+    def test_varasto_negatiivinen_varasto_luo_tyhjan_varaston(self):
+        uusiVarasto = Varasto(-10)
+        self.assertAlmostEqual(uusiVarasto.tilavuus,0)
+    
+    def test_varasto_negatiivinen_alkusaldo_luoo_tyhjan_alkusaldon(self):
+        uusiVarasto = Varasto(-10,-10)
+        self.assertAlmostEqual(uusiVarasto.saldo, 0)
+                
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_varastoon_lisataan_liian_paljon_tavaraa(self):
+        self.varasto.lisaa_varastoon(121)
+        self.assertAlmostEqual(self.varasto.tilavuus, self.varasto.saldo)
+    
+    def test_varastosta_otetaan_liian_paljon(self):
+        self.varasto.lisaa_varastoon(6)
+        self.assertAlmostEqual(self.varasto.ota_varastosta(10),6)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_str_toimii(self):
+        self.assertEqual(self.varasto.__str__(), "saldo = 0, vielä tilaa 10")
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
-
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
@@ -33,7 +56,6 @@ class TestVarasto(unittest.TestCase):
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.varasto.ota_varastosta(2)
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
