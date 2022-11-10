5d963effb28016a59ba2d5417b2470b7a48b5d13
astranero <esa.taheresa@gmail.com>
2022-11-10 13:45:02 +0200

    viikko 2 teht 5 done

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-
+print(f"{summa(x,y)}")
+print(f"{erotus(x,y)}")
 logger("lopetetaan")
